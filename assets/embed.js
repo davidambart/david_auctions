32a5a395c7bd10f65f3eb7b94a93c7dda67d646d
davidambart <david@brushstrokesintime.com>
2026-08-26 03:29:48 +0200

    Spread dark mode loader stars

--- assets/embed.js
+++ assets/embed.js
@@ -316,7 +316,7 @@
       const centerY = height / 2;
       const scale = Math.min(width, height);
       const outerRadius = scale * .43;
-      const ringDepth = outerRadius * .4;
+      const ringDepth = outerRadius * .43;
       context.save();
       context.globalCompositeOperation = 'lighter';
       stars.forEach(star => {
